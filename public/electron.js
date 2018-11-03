96a3d750a68e008257af1a516094456af0085b03
magoo <mael.gouacide@gmail.com>
2018-11-03 11:35:48 +0100

    update CI

--- public/electron.js
+++ public/electron.js
@@ -11,7 +11,7 @@ const isDev =
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({ width: 900, height: 680 });
+  mainWindow = new BrowserWindow({ width: 1200, height: 800 });
   mainWindow.loadURL(
     isDev
       ? 'http://localhost:3000'
