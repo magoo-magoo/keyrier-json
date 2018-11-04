9306f56d5a097ff1d9fe8d9e1355b6dafbd4fadf
magoo <mael.gouacide@gmail.com>
2018-11-04 09:50:43 +0100

    update CI

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,10 +1,18 @@
 import * as React from 'react';
+import { version } from '../../package.json';
 
 export const Header = () => (
   <nav className="navbar navbar-expand-md bg-primary">
     <a href="/" className="navbar-brand">
       Keyrier JSON
     </a>
+    <ul className="mr-auto navbar-nav">
+      <li className="nav-item">
+        <a href="#" className="nav-link">
+          {version}
+        </a>
+      </li>
+    </ul>
     <ul className="ml-auto navbar-nav">
       <li className="nav-item">
         <a
