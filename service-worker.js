4b6bf83cd2099178519aaf86df97db19f9613a82
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-13 00:44:55 +0000

    Deploy magoo-magoo/keyrier-json to github.com/magoo-magoo/keyrier-json.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.7415f55dc0b5a02a6d6e7dabf9959fa3.js"
+  "./precache-manifest.b4df91ce0cddb4168331ecc8766a749f.js"
 );
 
 self.addEventListener('message', (event) => {
