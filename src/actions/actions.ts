12000a5040710681bf1b8caa43ee18d84106caf8
magoo <mael.gouacide@gmail.com>
2019-08-09 23:47:38 +0200

    improve excel export

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -2,112 +2,107 @@ import { QueryMode, tabType } from 'state/State'
 import { GeneralTheme, EditorTheme } from 'themes/themes'
 
 export const updateAutoFormatSource = (active: boolean) =>
-  ({
-    active,
-    type: 'UPDATE_AUTOFORMAT_SOURCE',
-  } as const)
+    ({
+        active,
+        type: 'UPDATE_AUTOFORMAT_SOURCE',
+    } as const)
 
 export const updateSource = (source: string) =>
-  ({
-    source,
-    type: 'UPDATE_SOURCE_TEXT',
-  } as const)
+    ({
+        source,
+        type: 'UPDATE_SOURCE_TEXT',
+    } as const)
 
 export const updateQuery = (query: string) =>
-  ({
-    query,
-    type: 'UPDATE_QUERY',
-  } as const)
+    ({
+        query,
+        type: 'UPDATE_QUERY',
+    } as const)
 
 export const resetEditor = () =>
-  ({
-    type: 'RESET_EDITOR',
-  } as const)
+    ({
+        type: 'RESET_EDITOR',
+    } as const)
 
 export const undo = () =>
-  ({
-    type: 'APP_UNDO',
-  } as const)
+    ({
+        type: 'APP_UNDO',
+    } as const)
 
 export const redo = () =>
-  ({
-    type: 'APP_REDO',
-  } as const)
+    ({
+        type: 'APP_REDO',
+    } as const)
 
 export const clearEditor = () =>
-  ({
-    type: 'CLEAR_EDITOR',
-  } as const)
+    ({
+        type: 'CLEAR_EDITOR',
+    } as const)
 
 export const evaluateCode = () =>
-  ({
-    type: 'EVALUATE_CODE',
-  } as const)
+    ({
+        type: 'EVALUATE_CODE',
+    } as const)
 
 export const toggleOutputTableModal = () =>
-  ({
-    type: 'TOGGLE_OUTPUT_TABLE_MODAL',
-  } as const)
+    ({
+        type: 'TOGGLE_OUTPUT_TABLE_MODAL',
+    } as const)
 
 export const updateTableColumns = (columns: string[]) =>
-  ({
-    columns,
-    type: 'UPDATE_TABLE_COLUMNS',
-  } as const)
+    ({
+        columns,
+        type: 'UPDATE_TABLE_COLUMNS',
+    } as const)
 
 export const updateTableGroupBy = (groupBy: string[]) =>
-  ({
-    groupBy,
-    type: 'UPDATE_TABLE_GROUP_BY',
-  } as const)
+    ({
+        groupBy,
+        type: 'UPDATE_TABLE_GROUP_BY',
+    } as const)
 
 export const switchTheme = (theme: GeneralTheme) => ({
-  theme,
-  type: 'SWITCH_GLOBAL_THEME' as const,
+    theme,
+    type: 'SWITCH_GLOBAL_THEME' as const,
 })
 
 export const switchEditorTheme = (theme: EditorTheme) => ({
-  theme,
-  type: 'SWITCH_EDITOR_THEME' as const,
+    theme,
+    type: 'SWITCH_EDITOR_THEME' as const,
 })
 
 export const updateSearchTerm = (searchTerm: string) =>
-  ({
-    searchTerm,
-    type: 'UPDATE_OUTPUT_SEARCH_TERM',
-  } as const)
+    ({
+        searchTerm,
+        type: 'UPDATE_OUTPUT_SEARCH_TERM',
+    } as const)
 
 export const updateQueryMode = (mode: QueryMode) =>
-  ({
-    mode,
-    type: 'UPDATE_QUERY_MODE',
-  } as const)
+    ({
+        mode,
+        type: 'UPDATE_QUERY_MODE',
+    } as const)
 
 export const updateOutputTabSelection = (tab: tabType) =>
-  ({
-    tab,
-    type: 'UPDATE_OUTPUT_TAB_SELECTION',
-  } as const)
-
-type ReduxInitAction = {
-  type: '@@INIT'
-}
+    ({
+        tab,
+        type: 'UPDATE_OUTPUT_TAB_SELECTION',
+    } as const)
 
 export type Action =
-  | Readonly<ReduxInitAction>
-  | ReturnType<typeof evaluateCode>
-  | ReturnType<typeof resetEditor>
-  | ReturnType<typeof clearEditor>
-  | ReturnType<typeof updateQuery>
-  | ReturnType<typeof toggleOutputTableModal>
-  | ReturnType<typeof updateTableColumns>
-  | ReturnType<typeof updateTableGroupBy>
-  | ReturnType<typeof switchTheme>
-  | ReturnType<typeof updateSearchTerm>
-  | ReturnType<typeof updateQueryMode>
-  | ReturnType<typeof updateOutputTabSelection>
-  | ReturnType<typeof updateSource>
-  | ReturnType<typeof updateAutoFormatSource>
-  | ReturnType<typeof switchEditorTheme>
-  | ReturnType<typeof undo>
-  | ReturnType<typeof redo>
+    | ReturnType<typeof evaluateCode>
+    | ReturnType<typeof resetEditor>
+    | ReturnType<typeof clearEditor>
+    | ReturnType<typeof updateQuery>
+    | ReturnType<typeof toggleOutputTableModal>
+    | ReturnType<typeof updateTableColumns>
+    | ReturnType<typeof updateTableGroupBy>
+    | ReturnType<typeof switchTheme>
+    | ReturnType<typeof updateSearchTerm>
+    | ReturnType<typeof updateQueryMode>
+    | ReturnType<typeof updateOutputTabSelection>
+    | ReturnType<typeof updateSource>
+    | ReturnType<typeof updateAutoFormatSource>
+    | ReturnType<typeof switchEditorTheme>
+    | ReturnType<typeof undo>
+    | ReturnType<typeof redo>
--- src/components/output/TableAdvancedOptions.tsx
+++ src/components/output/TableAdvancedOptions.tsx
@@ -10,97 +10,100 @@ import { memo, useCallback, Suspense, ChangeEvent } from 'react'
 import { withErrorBoundary } from 'components/common/ErrorBoundary'
 import { lazy, FC } from 'react'
 import { Loading } from 'components/common/Loading'
+import _ from 'lodash'
 
 export const ReactSelect = lazy(() => import(/* webpackChunkName: "react-select" */ 'react-select'))
 
 interface Props {
-  data: itemType[]
-  displayedColumns: string[]
-  groupBy: string[]
-  columns: string[]
-  onColumnsChange: typeof updateTableColumns
-  setTableGroupBy: typeof updateTableGroupBy
+    data: itemType[]
+    displayedColumns: string[]
+    groupBy: string[]
+    columns: string[]
+    onColumnsChange: typeof updateTableColumns
+    setTableGroupBy: typeof updateTableGroupBy
 }
 
 const TableAdvancedOptions: FC<Props> = ({ onColumnsChange, columns, setTableGroupBy, data, displayedColumns }) => {
-  const [optionsCollapsed, switchOptionsCollapsed] = useToggleState()
+    const [optionsCollapsed, switchOptionsCollapsed] = useToggleState()
 
-  const handleColumnChange = useCallback(
-    (cols: ValueType<{}> | undefined | null) => {
-      if (cols instanceof Array) {
-        const mapped = cols.map((c: { value?: string }) => (c.value ? c.value : ''))
-        onColumnsChange(mapped)
-      }
-    },
-    [onColumnsChange]
-  )
+    const handleColumnChange = useCallback(
+        (cols: ValueType<{}> | undefined | null) => {
+            if (cols instanceof Array) {
+                const mapped = cols.map((c: { value?: string }) => (c.value ? c.value : ''))
+                onColumnsChange(mapped)
+            }
+        },
+        [onColumnsChange]
+    )
 
-  const handleExport = useCallback(async () => {
-    const xlsx = await import(/* webpackChunkName: "xlsx.js" */ 'xlsx')
-    const workBook = xlsx.utils.book_new()
-    const workSheet = xlsx.utils.json_to_sheet(data)
-    xlsx.utils.book_append_sheet(workBook, workSheet, 'keyrier-json')
-    xlsx.writeFile(workBook, 'export.xlsx')
-  }, [data])
+    const handleExport = useCallback(async () => {
+        const xlsx = await import(/* webpackChunkName: "xlsx.js" */ 'xlsx')
+        const workBook = xlsx.utils.book_new()
+        const workSheet = xlsx.utils.json_to_sheet(data.map(x => _.pick(x, displayedColumns)), {
+            header: displayedColumns,
+        })
+        xlsx.utils.book_append_sheet(workBook, workSheet, 'keyrier-json')
+        xlsx.writeFile(workBook, `export-${new Date().toISOString()}.xlsx`)
+    }, [displayedColumns, data])
 
-  const handleGroupChange = useCallback((e: ChangeEvent<HTMLSelectElement>) => setTableGroupBy([e.target.value]), [
-    setTableGroupBy,
-  ])
+    const handleGroupChange = useCallback((e: ChangeEvent<HTMLSelectElement>) => setTableGroupBy([e.target.value]), [
+        setTableGroupBy,
+    ])
 
-  if (columns.length <= 0) {
-    return <></>
-  }
+    if (columns.length <= 0) {
+        return <></>
+    }
 
-  const columnOptions = columns.map(k => ({ value: k, label: k }))
+    const columnOptions = columns.map(k => ({ value: k, label: k }))
 
-  return (
-    <div className="row">
-      <div className="col">
-        <Button className={'float-left'} color="primary" block={true} onClick={switchOptionsCollapsed}>
-          {optionsCollapsed ? 'Hide advanced options' : 'Advanced options'}
-        </Button>
-        <Collapse isOpen={optionsCollapsed}>
-          <select
-            className="form-control-lg form-control"
-            name="select"
-            id="groupingSelect"
-            onChange={handleGroupChange}
-          >
-            <option key={'Group by...'}>Group by...</option>
-            {displayedColumns.map(key => (
-              <option key={key}>{key}</option>
-            ))}
-          </select>
-          <Button color={'success'} onClick={handleExport}>
-            Export to Excel (.xlsx)
-          </Button>
-          <Suspense fallback={<Loading componentName="ReactSelect" />}>
-            <ReactSelect
-              options={columnOptions}
-              value={displayedColumns.map(k => ({
-                value: k,
-                label: k,
-              }))}
-              isMulti={true}
-              onChange={handleColumnChange}
-            />
-          </Suspense>
-        </Collapse>
-      </div>
-    </div>
-  )
+    return (
+        <div className="row">
+            <div className="col">
+                <Button className={'float-left'} color="primary" block={true} onClick={switchOptionsCollapsed}>
+                    {optionsCollapsed ? 'Hide advanced options' : 'Advanced options'}
+                </Button>
+                <Collapse isOpen={optionsCollapsed}>
+                    <select
+                        className="form-control-lg form-control"
+                        name="select"
+                        id="groupingSelect"
+                        onChange={handleGroupChange}
+                    >
+                        <option key={'Group by...'}>Group by...</option>
+                        {displayedColumns.map(key => (
+                            <option key={key}>{key}</option>
+                        ))}
+                    </select>
+                    <Button color={'success'} onClick={handleExport}>
+                        Export to Excel (.xlsx)
+                    </Button>
+                    <Suspense fallback={<Loading componentName="ReactSelect" />}>
+                        <ReactSelect
+                            options={columnOptions}
+                            value={displayedColumns.map(k => ({
+                                value: k,
+                                label: k,
+                            }))}
+                            isMulti={true}
+                            onChange={handleColumnChange}
+                        />
+                    </Suspense>
+                </Collapse>
+            </div>
+        </div>
+    )
 }
 
 const mapStateToProps = (state: RootState) => {
-  return {
-    data: getOutputarray(state),
-    displayedColumns: getdisplayedColumns(state),
-    columns: getColumns(state),
-    groupBy: getGroupBy(state),
-  }
+    return {
+        data: getOutputarray(state),
+        displayedColumns: getdisplayedColumns(state),
+        columns: getColumns(state),
+        groupBy: getGroupBy(state),
+    }
 }
 
 export default connect(
-  mapStateToProps,
-  { onColumnsChange: updateTableColumns, setTableGroupBy: updateTableGroupBy }
+    mapStateToProps,
+    { onColumnsChange: updateTableColumns, setTableGroupBy: updateTableGroupBy }
 )(memo(withErrorBoundary(TableAdvancedOptions)))
